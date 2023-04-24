ddd8e29e5028d19e41e663235ae2c9c285b49ce2
Santhosh Raj Selvaraj <93460395+santy1896@users.noreply.github.com>
2023-04-24 15:03:31 +0530

    Update passport.js
    remove some

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,7 @@ passport.deserializeUser(function (id, done) {
 function validateEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(email)) {
-        if (email.indexOf("@somaiya.edu", email.length - "@somaiya.edu".length) !== -1) {
+        if (email.indexOf("@gmail.com", email.length - "@gmail.com".length) !== -1) {
             return 1
         }
     }
@@ -73,7 +73,7 @@ passport.use('local-register', new LocalStrategy({
             return done(null, false, req.flash('error', messages));
         }
         else if (!validateEmail(email)) {
-            messages.push("Email Domain: @somaiya.edu required")
+            messages.push("Email Domain: @gmail.com required")
             return done(null, false, req.flash('error', messages));
         }
         else if ((validatePassword(password)).length != 0) {
